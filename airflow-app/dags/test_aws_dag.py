5dc637e8be68758595a429667c4ec592dc03a2f8
Otavio Cals <otaviolcals@gmail.com>
2022-02-10 12:26:06 -0300

    Update dag config

--- airflow-app/dags/test_aws_dag.py
+++ airflow-app/dags/test_aws_dag.py
@@ -34,24 +34,46 @@ with DAG(
         database="klarna_case",
     )
 
-    train_config = transform_config_from_estimator(
-        estimator=SKLearn(
-            entry_point="test.py",
-            source_dir="s3://klarna-case-model-bucket/credit-model/code/credit-model.tar.gz",
-            # role=role,
-            framework_version="0.23-1",
-            instance_type="ml.c4.xlarge",
-        ),
-        task_id="tf_training",
-        task_type="training",
-        instance_count=1,
-        instance_type="ml.m4.xlarge",
-        data="s3://klarna-case-model-bucket/credit-model/train/raw-train-data/raw-train-data.csv",
-        content_type="text/csv",
-    )
-
     train_op = SageMakerTrainingOperator(
-        task_id="test_training", config=train_config, wait_for_completion=True, dag=dag
+        task_id="test_training",
+        config={
+            "AlgorithmSpecification": {
+                "TrainingImage": "683313688378.dkr.ecr.us-east-1.amazonaws.com/sagemaker-scikit-learn:0.23-1-cpu-py3",
+                "TrainingInputMode": "File",
+            },
+            "OutputDataConfig": {
+                "S3OutputPath": "s3://sagemaker-us-east-1-025105345127/"
+            },
+            "StoppingCondition": {"MaxRuntimeInSeconds": 86400},
+            "ResourceConfig": {
+                "InstanceCount": 1,
+                "InstanceType": "ml.c4.xlarge",
+                "VolumeSizeInGB": 30,
+            },
+            "RoleArn": "test",
+            "InputDataConfig": [
+                {
+                    "DataSource": {
+                        "S3DataSource": {
+                            "S3DataType": "S3Prefix",
+                            "S3Uri": "s3://klarna-case-model-bucket/credit-model/train/raw-train-data/raw-train-data.csv",
+                            "S3DataDistributionType": "FullyReplicated",
+                        }
+                    },
+                    "ChannelName": "training",
+                }
+            ],
+            "HyperParameters": {
+                "sagemaker_submit_directory": '"s3://klarna-case-model-bucket/credit-model/code/credit-model.tar.gz"',
+                "sagemaker_program": '"test.py"',
+                "sagemaker_container_log_level": "20",
+                "sagemaker_job_name": '"sagemaker-scikit-learn-2022-02-10-15-24-04-586"',
+                "sagemaker_region": '"us-east-1"',
+            },
+            "TrainingJobName": "sagemaker-scikit-learn-2022-02-10-15-24-04-586",
+        },
+        wait_for_completion=True,
+        dag=dag,
     )
 
 

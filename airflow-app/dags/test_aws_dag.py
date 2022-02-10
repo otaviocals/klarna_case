1f87f4b53fb6b62289a200c386a14fd06a010a23
Otavio Cals <otaviolcals@gmail.com>
2022-02-10 09:45:00 -0300

    Add sagemaker train test

--- airflow-app/dags/test_aws_dag.py
+++ airflow-app/dags/test_aws_dag.py
@@ -5,8 +5,13 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.dummy import DummyOperator
 from airflow.utils.dates import days_ago
 from airflow.providers.amazon.aws.operators.athena import AWSAthenaOperator
+from airflow.contrib.operators.sagemaker_training_operator import (
+    SageMakerTrainingOperator,
+)
 
 import boto3
+from sagemaker.workflow.airflow import transform_config_from_estimator
+from sagemaker.sklearn.estimator import SKLearn
 
 args = {
     "owner": "airflow",
@@ -25,12 +30,32 @@ with DAG(
     test_athena_operator = AWSAthenaOperator(
         task_id=f"run_query_test",
         query="select * from credit_train_data where has_paid=TRUE",
-        output_location="s3://klarna-case-model-bucket/credit-model/train/raw-train-data/",
+        output_location="s3://klarna-case-model-bucket/credit-model/train/raw-train-data/raw-train-data.csv",
         database="klarna_case",
     )
 
+    train_config = transform_config_from_estimator(
+        estimator=SKLearn(
+            entry_point="test.py",
+            source_dir="s3://klarna-case-model-bucket/credit-model/code/credit-model.tar.gz",
+            # role=role,
+            framework_version="0.23-1",
+            instance_type="ml.c4.xlarge",
+        ),
+        task_id="tf_training",
+        task_type="training",
+        instance_count=1,
+        instance_type="ml.m4.xlarge",
+        data="s3://klarna-case-model-bucket/credit-model/train/raw-train-data/raw-train-data.csv",
+        content_type="text/csv",
+    )
 
-test_athena_operator
+    train_op = SageMakerTrainingOperator(
+        task_id="test_training", config=train_config, wait_for_completion=True, dag=dag
+    )
+
+
+test_athena_operator >> train_op
 
 if __name__ == "__main__":
     dag.cli()

91e5a85e7f942142949a5ff0ca1af0321956174b
Otavio Cals <otaviolcals@gmail.com>
2022-02-11 21:09:38 -0300

    Add deploy operator

--- airflow-app/dags/test_aws_dag.py
+++ airflow-app/dags/test_aws_dag.py
@@ -51,18 +51,19 @@ with DAG(
         dag=dag,
     )
 
-    create_model = SageMakerModelOperator(
-        task_id="create_new_model",
-        config=generate_sagemaker_model_config(
-            "credit-model",
-            "credit-model.train",
-            "test_training",
-        ),
-        dag=dag,
+    deploy_model = DeployModelOperator(
+        task_id="deploy_model",
+        model_bucket="klarna-case-model-bucket",
+        model_location="credit-model/model/model.joblib",
+        new_version_location="{{ ti.xcom_pull(task_ids='test_training', key='return_value')['Training']['ModelArtifacts']['S3ModelArtifacts'].strip('\\\"').strip('s3://klarna-case-model-bucket/') }}",
+        model_filename="model.joblib",
+        restart_model_webserver=False,
+        master_internal_ip=None,
+        master_model_resource_path=None,
     )
 
 
-test_athena_operator >> train_op >> create_model
+test_athena_operator >> train_op >> deploy_model
 
 if __name__ == "__main__":
     dag.cli()
